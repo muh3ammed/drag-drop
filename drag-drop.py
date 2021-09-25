64fce6bd157c6777f9bcb4b060a360bb62065e5f
Muhammed Mahdi <64995259+muh3ammed@users.noreply.github.com>
2021-09-25 13:10:11 +0300

    Simple drag&drop with pyqt5

--- drag-drop.py
+++ drag-drop.py
@@ -5,7 +5,9 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 class App(QMainWindow):
 
     def __init__(self):
+        
         super().__init__()
+        
         self.title = 'DRAG AND DROP'
         self.setWindowTitle(self.title)
         self.resize(400,400)
@@ -14,20 +16,27 @@ class App(QMainWindow):
         self.setAcceptDrops(True)
 
     def dragEnterEvent(self, event):
+        
     	if event.mimeData().hasUrls():
+            
     		event.accept()
+            
     	else:
+            
     		event.ignore()
 
     def dropEvent(self, event):
+        
     	files = [u.toLocalFile() for u in event.mimeData().urls()]
 
     	for file in files:
+            
     		image = cv2.imread(file)
     		cv2.imshow('image', image)
     		cv2.waitKey(0)       
     
 if __name__ == '__main__':
+    
     app = QApplication(sys.argv)
     window = App()
     window.show()
